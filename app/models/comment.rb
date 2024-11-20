335ae16e5a5e7765a9e9c38ccbb6b50ceac861f0
dreadlord1377 <ilia.taranenko@gmail.com>
2024-11-20 20:10:53 +0300

    Tabulation has been fixed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
-	include Visible
+  include Visible
 
-	belongs_to :article
-	
-	validates :commenter, presence: true, length: { minimum: 1 }
-	validates :body, presence: true, length: { minimum: 1 }
+  belongs_to :article
+
+  validates :commenter, presence: true, length: { minimum: 1 }
+  validates :body, presence: true, length: { minimum: 1 }
 end
--- db/migrate/20241113150238_create_articles.rb
+++ db/migrate/20241113150238_create_articles.rb
@@ -1,10 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[8.0]
-	def change
-		create_table :articles do |t|
-			t.string :title
-			t.text :body
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
 
-			t.timestamps
-		end
-	end
+      t.timestamps
+    end
+  end
 end
