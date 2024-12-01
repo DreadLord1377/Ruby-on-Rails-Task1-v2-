ed071b793009a5ac0a1c4262c2eb1b9f27539db2
dreadlord1377 <ilia.taranenko@gmail.com>
2024-12-01 20:47:32 +0300

    rspec add

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,50 +1,45 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "DreadLord1377", password: "admin", except: [ :index, :show ]
-
+  
   def index
     @articles = Article.all
+
+    render json: @articles
   end
 
   def show
-    @article = Article.find(params[:id])
-  end
-
-  def new
-    @article = Article.new
+    set_article
+    render json: [@article, @article.comments]
   end
 
   def create
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to @article
+      render json: @article, status: :created
     else
-      render :new, status: :unprocessable_entity
+      render json: @article.errors, status: :unprocessable_entity
     end
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
-
   def update
-    @article = Article.find(params[:id])
-
+    set_article
     if @article.update(article_params)
-      redirect_to @article
+      render json: @article
     else
-      render :edit, status: :unprocessable_entity
+      render json: @article.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    set_article
     @article.destroy
-
-    redirect_to root_path, status: :see_other
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.expect(article: [ :title, :body, :status ])
     end
