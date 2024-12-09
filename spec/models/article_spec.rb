94fbdbcfb98d5f031e4f383de2057bff3d19fade
dreadlord1377 <ilia.taranenko@gmail.com>
2024-12-10 02:32:10 +0300

    Tab fix

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,28 +1,28 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
-	it "is valid with valid params" do
-		@article = Article.new(title: "Test", body: "Testing...", status: "archived")
-		expect(@article).to be_valid
-	end
+  it "is valid with valid params" do
+    @article = Article.new(title: "Test", body: "Testing...", status: "archived")
+    expect(@article).to be_valid
+  end
 
-	it "is not valid with not valid title" do
-		@article = Article.new(body: "Testing...", status: "public")
-		expect(@article).not_to be_valid
-	end
+  it "is not valid with not valid title" do
+    @article = Article.new(body: "Testing...", status: "public")
+    expect(@article).not_to be_valid
+  end
 
-	it "is not valid with not valid body" do
-		@article = Article.new(title: "Test article", body: "trying", status: "private")
-		expect(@article).not_to be_valid
-	end
+  it "is not valid with not valid body" do
+    @article = Article.new(title: "Test article", body: "trying", status: "private")
+    expect(@article).not_to be_valid
+  end
 
-	it "is not valid with not valid status" do
-		@article = Article.new(title: "Test article", body: "trying...")
-		expect(@article).not_to be_valid
-	end
+  it "is not valid with not valid status" do
+    @article = Article.new(title: "Test article", body: "trying...")
+    expect(@article).not_to be_valid
+  end
 
-	it "is not valid with not valid params" do
-		@article = Article.new(body: "trying", status: "public")
-		expect(@article).not_to be_valid
-	end
+  it "is not valid with not valid params" do
+    @article = Article.new(body: "trying", status: "public")
+    expect(@article).not_to be_valid
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,38 +1,38 @@
 require "rails_helper"
 
 RSpec.describe Comment, type: :model do
-	it "is valid with valid params" do
-		@article = Article.new(title: "Test", body: "Testing...", status: "public")
-		@article.save
-		@comment = @article.comments.create(commenter: "Developer", body: "Testing comment...", status: "public")
-		expect(@comment).to be_valid
-	end
+  it "is valid with valid params" do
+    @article = Article.new(title: "Test", body: "Testing...", status: "public")
+    @article.save
+    @comment = @article.comments.create(commenter: "Developer", body: "Testing comment...", status: "public")
+    expect(@comment).to be_valid
+  end
 
-	it "is not valid with not valid status" do
-		@article = Article.new(title: "Test", body: "Testing...", status: "public")
-		@article.save
-		@comment = @article.comments.create(commenter: "Developer", body: "Testing comment...")
-		expect(@comment).not_to be_valid
-	end
+  it "is not valid with not valid status" do
+    @article = Article.new(title: "Test", body: "Testing...", status: "public")
+    @article.save
+    @comment = @article.comments.create(commenter: "Developer", body: "Testing comment...")
+    expect(@comment).not_to be_valid
+  end
 
-	it "is not valid with not valid title" do
-		@article = Article.new(title: "Test", body: "Testing...", status: "archived")
-		@article.save
-		@comment = @article.comments.create(body: "Testing comment...", status: "public")		
-		expect(@comment).not_to be_valid
-	end
+  it "is not valid with not valid title" do
+    @article = Article.new(title: "Test", body: "Testing...", status: "archived")
+    @article.save
+    @comment = @article.comments.create(body: "Testing comment...", status: "public")   
+    expect(@comment).not_to be_valid
+  end
 
-	it "is not valid with not valid body" do
-		@article = Article.new(title: "Test", body: "Testing...", status: "private")
-		@article.save
-		@comment = @article.comments.create(commenter: "Developer", body: "", status: "public")
-		expect(@comment).not_to be_valid
-	end
+  it "is not valid with not valid body" do
+    @article = Article.new(title: "Test", body: "Testing...", status: "private")
+    @article.save
+    @comment = @article.comments.create(commenter: "Developer", body: "", status: "public")
+    expect(@comment).not_to be_valid
+  end
 
-	it "is not valid with not valid params" do
-		@article = Article.new(title: "Test", body: "Testing...", status: "public")
-		@article.save
-		@comment = @article.comments.create()
-		expect(@comment).not_to be_valid
-	end
+  it "is not valid with not valid params" do
+    @article = Article.new(title: "Test", body: "Testing...", status: "public")
+    @article.save
+    @comment = @article.comments.create()
+    expect(@comment).not_to be_valid
+  end
 end
