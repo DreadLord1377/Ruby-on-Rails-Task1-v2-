f1dace5d5b2758de499ff959783644a84da1f1f5
dreadlord1377 <ilia.taranenko@gmail.com>
2025-01-06 14:20:52 +0300

    Validation, creation and update callbacks added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,28 @@
 class Article < ApplicationRecord
+  #JSON_SCHEMA = "#{Rails.root}/app/spec/swagger_helper.rb"
+
   include Visible
 
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 1 }
   validates :body, presence: true, length: { minimum: 10 }
+
+  #validation callback
+  after_validation :log_errors
+
+  #creation callback
+  after_create :log_errors
+
+  #update callback
+  after_update :log_errors
+
+  private
+    def log_errors
+      if errors.any?
+        Rails.logger.error("Validation failed: #{errors.full_messages.join(', ')}")
+      else
+        Rails.logger.info("Successful request")
+      end
+    end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,22 @@ class Comment < ApplicationRecord
 
   validates :commenter, presence: true, length: { minimum: 1 }
   validates :body, presence: true, length: { minimum: 1 }
+
+  #validation callback
+  after_validation :log_errors
+
+  #creation callback
+  after_create :log_errors
+
+  #update callback
+  after_update :log_errors
+
+  private
+    def log_errors
+      if errors.any?
+        Rails.logger.error("Validation failed: #{errors.full_messages.join(', ')}")
+      else
+        Rails.logger.info("Successful request")
+      end
+    end
 end
